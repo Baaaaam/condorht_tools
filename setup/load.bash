fc6df487a762de3c44876656076264fd68f60b37
MOUGINOT B <mouginot.baptiste@gmail.com>
2017-02-02 22:44:18 -0600

    update load

--- setup/load.bash
+++ setup/load.bash
@@ -17,7 +17,7 @@ rm -rf $build_dir $install_dir
 mkdir -p $dist_dir $build_dir $install_dir $copy_dir $DATAPATH
 
 # Make sure all the dependencies are built
-packages=(mcnp smure)
+packages=(mcnp mure)
 for name in "${packages[@]}"; do
   eval version=\$"$name"_version
   echo Ensuring build of $name-$version ...
@@ -25,10 +25,9 @@ for name in "${packages[@]}"; do
 done
 echo $PATH
 
-mcnp6
-
-
-cp $dist_dir/REP_MOX/REP_MOX.tar .
-tar -xvf REP_MOX.tar
-g++ -o MOX MOX.cc -I$MURE_include -I$MURE_ExternalPkg -L$MURE_lib -lMUREpkg -lvalerr -lmctal -fopenmp
-./MOX 1 2 3 4 5 6 7 7
+mkdir MOX
+cd MOX
+cp -v /mnt/gluster/mouginot/MOX/*.* .
+cp -r /mnt/gluster/mouginot/MOX/ReactionList .
+g++  -o MOX MOX.cc -I$MURE_include -I$MURE_ExternalPkg -L$MURE_lib -lMUREpkg -lvalerr -lmctal -fopenmp
+./MOX 0.03 0.5 5 19 5 5 6 4 2 5 1 2 5 5 5 5
